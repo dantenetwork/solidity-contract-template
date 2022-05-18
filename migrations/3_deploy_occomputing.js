ad41f5b37710dff3d1b56b61f7da7c9ed7d18985
virgil2019 <danten0402@gmail.com>
2022-05-18 10:48:33 +0800

    Change migration of OC

--- migrations/3_deploy_occomputing.js
+++ migrations/3_deploy_occomputing.js
@@ -4,4 +4,11 @@ const fs = require("fs");
 module.exports = async function (deployer) {
   await deployer.deploy(OCComputing);
   console.log('OCComputing contract address: ' + OCComputing.address);
+
+  // generating address.json
+  const contractAddressFile = './build/oc.json';
+  const contractAddress = {
+    'OCContractAddress': OCComputing.address
+  };
+  fs.writeFileSync(contractAddressFile, JSON.stringify(contractAddress));
 };
