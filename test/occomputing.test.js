56beee641c9720c13bbb9062c489290f5e33eec4
virgil2019 <danten0402@gmail.com>
2022-06-30 14:55:58 +0800

    Change test case

--- test/occomputing.test.js
+++ test/occomputing.test.js
@@ -32,6 +32,7 @@ contract('OCComputing', function(accounts) {
 
         // register target
         await ocComputing.registerDestnContract('receiveComputeTask', 'PLATONEVMDEV', OCComputing.address, 'receiveComputeTask');
+        await ocComputing.registerDestnContract('receiveComputeTaskCallback', 'PLATONEVMDEV', OCComputing.address, 'receiveComputeTaskCallback');
     }
 
     before(async function() {
@@ -47,14 +48,14 @@ contract('OCComputing', function(accounts) {
     describe('Receive OC Computing Task', function() {
         it('should execute successfully', async () => {
             let to = OCComputing.address;
-            let action = '0x1db89088';
+            let action = '0x47e50a42';
             let item = {
                 name: 'nums',
                 msgType: 14,
                 value: '0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c0000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001400000000000000000000000000000000000000000000000000000000000000006504c41544f4e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000094772656574696e6773000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000144772656574696e672066726f6d20504c41544f4e00000000000000000000000000000000000000000000000000000000000000000000000000000000000000374d6f6e204a756e20323720323032322031363a33343a323820474d542b3038303020284368696e61205374616e646172642054696d6529000000000000000000',
             };
             let calldata = {items: [item]};
-            let argument = [1, 'PLATONEVMDEV', OCComputing.address, owner, [0], to, action, calldata, [0, '0x11111111'], 0];
+            let argument = [1, 'PLATONEVMDEV', OCComputing.address, owner, [0], to, action, calldata, [0, utils.toHexString(utils.stringToByteArray('0x11111111'))], 0];
             await crossChain.receiveMessage(argument, {from: user1});
             await crossChain.executeMessage('PLATONEVMDEV', 1);
             let context = await crossChain.currentSimplifiedMessage();
